ae0d32bc2f29eb4c2a3232002f7f233120971048
Ashim Rudra Paul <81712334+codewithashim@users.noreply.github.com>
2023-11-12 05:51:59 +0600

    dashboard is done

--- src/Components/Dashboard/Product/AddProduct/AddProduct.jsx
+++ src/Components/Dashboard/Product/AddProduct/AddProduct.jsx
@@ -7,10 +7,13 @@ import { Select } from 'antd';
 import Swal from "sweetalert2";
 import useProducts from '@/src/Hooks/useProducts';
 import { createProductUrl } from '@/src/Utils/Urls/ProductUrl';
+import { FaTrashAlt } from "react-icons/fa";
+
 
 
 const AddProduct = () => {
   const { handleSubmit } = useForm();
+  const [loading, setLoading] = useState(false);
   const { categoryData, couponData } = useProducts();
   const [category, setCategory] = useState("");
   const [coupon, setCoupon] = useState("");
@@ -18,14 +21,80 @@ const AddProduct = () => {
   const [price, setPrice] = useState("");
   const [discountPercentage, setDiscountPercentage] = useState("");
   const [quantity, setQuantity] = useState("");
-  const [description, setDescription] = useState("");
-  const [features, setFeatures] = useState([]);
   const [brand, setBrand] = useState("");
   const [type, setType] = useState("")
   const [status, setStatus] = useState("")
   const [isAndroid, setIsAndroid] = useState(false)
+  const [showAndroidDetails, setShowAndroidDetails] = useState(false);
+  const [processor, setProcessor] = useState("");
+  const [ram, setRam] = useState("");
+  const [rom, setRom] = useState("");
+  const [displaySize, setDisplaySize]= useState("")
+  const [wlcpaa, setWlcpaa] = useState("")
+  const [dvr, setDvr] = useState("")
+  const [sim, setSim] = useState("")
+  const [camera360, setCamera360] = useState("")
+  const [opticalInput, setOpticalInput] = useState("")
+  const [warranty, setWarranty] = useState("")
+  const [qled, setQled]= useState("")
 
-  const [loading, setLoading] = useState(false);
+  // ===
+  const productDetailTamplate = {
+    heading:"",
+    description: ""
+  }
+  const [description, setDescription] = useState([productDetailTamplate]);
+
+  const addDetails = ()=>{
+    setDescription([...description, productDetailTamplate])
+  }
+
+  const removeDetails = (index)=>{
+    const newDetails = description.filter((description, i) => i !== index);
+    setDescription(newDetails)
+  }
+
+  const onChangeDetail  = (event, index) => {
+    const updatedDetal = lend.map((lend, i) =>
+      index === i
+        ? Object.assign(lend, { [event.target.name]: event.target.value })
+        : lend
+    );
+    setDescription(updatedDetal);
+  };
+
+  const handleAndroidCheckboxChange = (e) => {
+    setIsAndroid(e.target.checked);
+    setShowAndroidDetails(e.target.checked);
+  };
+
+  const featuresTamplate = {
+    heading:"",
+    description: ""
+  }
+
+  const [features, setFeatures] = useState([featuresTamplate]);
+
+  const addFeatures = ()=>{
+    setFeatures([...features, featuresTamplate])
+  }
+
+  const removeFeatures = (index)=>{
+    const newFeatures = features.filter((features, i) => i !== index);
+    setFeatures(newFeatures)
+  }
+
+  const onChangeFeatures  = (event, index) => {
+    const updatedFeatures = lend.map((lend, i) =>
+      index === i
+        ? Object.assign(lend, { [event.target.name]: event.target.value })
+        : lend
+    );
+    setFeatures(updatedFeatures);
+  };
+
+
+
 
   // === coupon ===
   const couponOptions = couponData?.map((coupon) => ({
@@ -87,6 +156,22 @@ const AddProduct = () => {
           throw new Error('Failed to retrieve the image URL from Cloudinary response.');
         }
       }
+
+      const android = [{
+      isAndroid: isAndroid,
+      processor: processor,
+      ram:ram,
+      rom: rom,
+      displaySize: displaySize,
+      wlcpaa: wlcpaa,
+      dvr: dvr,
+      camera360: camera360,
+      sim: sim,
+      opticalInput:opticalInput,
+      qled: qled,
+      warranty:warranty 
+      }]
+
       const productData = {
         name: name,
         categories: category,
@@ -100,6 +185,7 @@ const AddProduct = () => {
         features: features,
         coupon: coupon,
         quantity: quantity,
+        android: android,
       }
 
       const res = await fetch(createProductUrl, {
@@ -158,8 +244,7 @@ const AddProduct = () => {
   return (
     <section className="my-4">
       <div className="flex flex-col w-full gap-4 mx-auto add-book-form">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
+        <div
           className="add-book-form w-full md:w-[60%] mx-auto flex flex-col gap-4 "
         >
           <Input
@@ -236,8 +321,187 @@ const AddProduct = () => {
             value={status}
             onChange={(e) => setStatus(e.target.value)}
           />
+          <div className='flex flex-col gap-4'>
+            <h1 className='my-2'>Description</h1>
+            {
+              description.map((description, index) => {
+                return(
+                  <section
+                  key={index}
+                  className=""
+                >
+                  <div className="form-control w-full">
+                    <div className='border p-2'>
+                    <input
+                      type="text"
+                      name="Heading"
+                      onChange={(event) => onChangeDetail(event, index)}
+                      value={description.heading}
+                      placeholder="Heading"
+                      className="input input-bordered w-full"
+                    />
+                    </div>
+                  </div>
+  
+                  <div className="form-control w-full my-2">
+                    <div className='border p-2'>
+                    <input
+                      type="text"
+                      name="description"
+                      onChange={(event) => onChangeDetail(event, index)}
+                      value={description.description}
+                      placeholder="Description"
+                      className="input input-bordered w-full border"
+                    />
+                    </div>
+                  </div>
+  
+  
+                  <div>
+                    <button
+                      className="common-btn flex items-center gap-2"
+                      onClick={() => removeDetails(index)}
+                    >
+                      <FaTrashAlt className="text-2xl mr-2"></FaTrashAlt>
+                      Delete
+                    </button>
+                  </div>
+                </section>
+                )
+              })
+            }
+          <button className="common-btn" onClick={() => addDetails()}>
+            Add More Details
+          </button>
+          </div>
+
+          <div className='flex flex-col gap-4'>
+          <h1 className='my-2'>Features</h1>
+            {
+              features.map((features, index) => {
+                return(
+                  <section
+                  key={index}
+                  className=""
+                >
+                  <div className="form-control w-full">
+                    <div className='border p-2'>
+                    <input
+                      type="text"
+                      name="Heading"
+                      onChange={(event) => onChangeFeatures(event, index)}
+                      value={features.heading}
+                      placeholder="Heading"
+                      className="input input-bordered w-full"
+                    />
+                    </div>
+                  </div>
+  
+                  <div className="form-control w-full my-2">
+                    <div className='border p-2'>
+                    <input
+                      type="text"
+                      name="description"
+                      onChange={(event) => onChangeFeatures(event, index)}
+                      value={features.description}
+                      placeholder="Description"
+                      className="input input-bordered w-full border"
+                    />
+                    </div>
+                  </div>
+  
+
+                  <div>
+                    <button
+                      className="common-btn flex items-center gap-2"
+                      onClick={() => removeFeatures(index)}
+                    >
+                      <FaTrashAlt className="text-2xl mr-2"></FaTrashAlt>
+                      Delete
+                    </button>
+                  </div>
+                </section>
+                )
+              })
+            }
+               <button className="common-btn" onClick={() => addFeatures()}>
+            Add More Features
+          </button>
+          </div>
+
 
           <div>
+          <label className='flex items-center'>
+            Is Android
+            <input
+              type="checkbox"
+              checked={isAndroid}
+              onChange={handleAndroidCheckboxChange}
+              className="border mx-2 my-4"
+            />
+          </label>
+
+          {showAndroidDetails && (
+            <div className='flex flex-col gap-4'>
+              <Input
+                placeholder="Processor"
+                value={processor}
+                onChange={(e) => setProcessor(e.target.value)}
+              />
+              <Input
+                placeholder="RAM"
+                value={ram}
+                onChange={(e) => setRam(e.target.value)}
+              />
+              <Input
+                placeholder="ROM"
+                value={rom}
+                onChange={(e) => setRom(e.target.value)}
+              />
+              <Input
+                placeholder="Display Size"
+                value={displaySize}
+                onChange={(e) => setDisplaySize(e.target.value)}
+              />
+              <Input
+                placeholder="WlcPaa"
+                value={wlcpaa}
+                onChange={(e) => setWlcpaa(e.target.value)}
+              />
+              <Input
+                placeholder="Camera360"
+                value={camera360}
+                onChange={(e) => setCamera360(e.target.value)}
+              />
+              <Input
+                placeholder="Dvr"
+                value={dvr}
+                onChange={(e) => setDvr(e.target.value)}
+              />
+              <Input
+                placeholder="Sim"
+                value={sim}
+                onChange={(e) => setSim(e.target.value)}
+              />
+              <Input
+                placeholder="Optical Input"
+                value={opticalInput}
+                onChange={(e) => setOpticalInput(e.target.value)}
+              />
+              <Input
+                placeholder="Qled"
+                value={qled}
+                onChange={(e) => setQled(e.target.value)}
+              />
+              <Input
+                placeholder="Warranty"
+                value={warranty}
+                onChange={(e) => setWarranty(e.target.value)}
+              />
+        </div>
+          )}
+
+
 
           </div>
 
@@ -295,14 +559,16 @@ const AddProduct = () => {
             </div>
           </div>
 
-          <Button type="default" htmlType="submit" style={{
+          <Button 
+          onClick={handleSubmit(onSubmit)}
+          type="default" htmlType="submit" style={{
             marginTop: '20px',
           }}>
             {
               loading ? 'Loading...' : 'Add Product'
             }
           </Button>
-        </form>
+        </div>
       </div>
     </section>
   );
