876d160417d48fe2a23765a39c6367c7dac6aedf
Bitopan <bitopanxon1@gmail.com>
2023-12-08 13:42:31 +0530

    Updatee-8-12-23

--- src/Components/Home/ExploreBestSeal/ExploreBestSeal.jsx
+++ src/Components/Home/ExploreBestSeal/ExploreBestSeal.jsx
@@ -69,7 +69,7 @@ const ExploreBestSeal = () => {
 
     return (
         <div className="container ">
-            <h3 className="font-light  text-3xl text-black">Trending <strong className='font-extrabold text-[#29679e]'>Products</strong></h3>
+            <h3 className="font-light mb-2 text-3xl text-black">Trending <strong className='font-extrabold text-[#29679e]'>Products</strong></h3>
             <Swiper
                 modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
                 breakpoints={{
@@ -96,7 +96,7 @@ const ExploreBestSeal = () => {
                     slidesPerView: 5,
                   },
                 }}
-                // spaceBetween={20}
+                spaceBetween={12}
                 // slidesPerView={3}
                 onSlideChange={() => { }}
                 onSwiper={(swiper) => { }}
@@ -108,7 +108,7 @@ const ExploreBestSeal = () => {
                 return (
                   <SwiperSlide key={itm.id}>
                     <div
-                      className="color-b  bg-white p-2 md:p-3 text-center rounded-md duration-300 transform  shadow-sm hover:-translate-y-1.5 border-t border-slate-100 hover:bg-red-10 hover:text-[#18568C]" 
+                      className="  bg-white p-2 md:p-3 text-center rounded-md duration-300 transform  shadow-sm hover:-translate-y-1.5 cardOutline hover:bg-red-10 hover:text-[#18568C]" 
                       data-aos="fade-up" // Add AOS animation attribute
                       data-aos-duration="1000" // Set animation duration in milliseconds
                       key={itm.id}
@@ -119,9 +119,35 @@ const ExploreBestSeal = () => {
                                 <video dblclick={e => e.preventDefault()} className="absolute  top-[0px]  left-0 bottom-0 right-0 duration-200 hover-img opacity-0 object-fit object-center sm:object-top md:object-top lg:object-top xl:object-top"  muted  loop autoPlay>
                                     <source src={itm?.video} type="video/mp4" />
                                 </video>
+                             
                             </div>
-                              <h3 className="text-[16px]  text-center font-semibold text-slate-700 mt-1">{itm?.name}</h3>
-                            
+                            <div className="bg-white z-[9999999999] absolute h-[3rem] w-[3rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2  p-1">                               
+                             <img src={itm?.thum} className="w-full h-full " />
+                            </div>
+
+                              <div>
+                              <h3 className="text-[16px]  text-center font-semibold text-slate-700 mt-6">{itm?.name}</h3>
+                              </div>
+                          <div>
+                           <div className="text-[16px] text-gray-900 mt-1 flex justify-between align-middle text-left">
+                             <div className='font-semibold'>
+                               {/* {itm?.discount
+                                 ? `₹ ${Math.floor(itm?.price - (itm?.price * itm?.discount) / 100)}`
+                                 : `₹ ${Math.floor(itm?.price)}`
+                               } */}
+                               $ 9,9999
+                               <span className="text-sm text-gray-400 line-through mx-2">
+                               {/* ₹ {Math.floor(itm?.price)} */}
+                               $21,900
+                              </span>
+                             </div>
+                             
+                             <span className='text-[#2f9555] text-bold'>
+                               {/* {Math.floor(itm?.discount)}% off */}
+                               55% off
+                             </span>
+                           </div>
+                         </div>
                         </Link>
                        
                     </div>
